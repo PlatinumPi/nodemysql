f35dba151e45c4ce2f38df465aee573d6300c091
PlatinumPi <goldpi101@gmail.com>
2019-05-05 19:18:14 -0600

    Proto A

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS nodestore;
+CREATE database nodestore;
+
+USE nodestore;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,4) NULL,
+  stock_quantity DECIMAL(10,4) NULL,
+  PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
