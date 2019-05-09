3c37de5ac0adbbf17d7d7702a325029e6575358d
PlatinumPi <goldpi101@gmail.com>
2019-05-09 12:24:00 -0600

    Proto B

--- nodestore.js
+++ nodestore.js
@@ -5,25 +5,18 @@ const inquirer = require("inquirer")
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "root",
     database: "nodestore"
 });
 
-// connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err
-        // run the displayInventory function after the connection is made to prompt the user
     displayInventory()
 });
 
 let displayInventory = () => {
-    //console.log("SQL Connection Established")
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err
         for (let i = 0; i < res.length; i++) {
             console.log(" - - - - - - - - - - - - - - - ")
@@ -35,7 +28,6 @@ let displayInventory = () => {
     })
 };
 
-// validateInput makes sure that the user is supplying only positive integers for their inputs
 let validateInput = (value) => {
     var integer = Number.isInteger(parseFloat(value))
     var sign = Math.sign(value)
@@ -47,30 +39,29 @@ let validateInput = (value) => {
     }
 }
 
-// purchase function to prompt the customer for an item to purchase
 let purchase = () => {
     inquirer.prompt([{
-                type: "input",
-                name: "item_id",
-                message: "Select the item you would like to purchase by item number.",
-                validate: validateInput,
-                filter: Number
-            },
-            {
-                type: "input",
-                name: "quantity",
-                message: "How many of this item would you like to purchase?",
-                validate: validateInput,
-                filter: Number
-            }
-        ])
-        .then(function(purchase) {
+        type: "input",
+        name: "item_id",
+        message: "Select the item you would like to purchase by item number.",
+        validate: validateInput,
+        filter: Number
+    },
+    {
+        type: "input",
+        name: "quantity",
+        message: "How many of this item would you like to purchase?",
+        validate: validateInput,
+        filter: Number
+    }
+    ])
+        .then(function (purchase) {
             let item = purchase.item_id
             let quantity = purchase.quantity
 
             let queryStr = 'SELECT * FROM products WHERE ?';
 
-            connection.query(queryStr, { item_id: item }, function(err, res) {
+            connection.query(queryStr, { item_id: item }, function (err, res) {
                 if (err) throw err
 
                 if (res.length === 0) {
@@ -78,19 +69,15 @@ let purchase = () => {
                     displayInventory()
                 } else {
 
-                    // set the results to the variable of productInfo
                     let productInfo = res[0]
 
                     if (quantity <= productInfo.stock_quantity) {
                         console.log(productInfo.product_name + "is in stock! Placing order now!")
                         console.log("\n")
 
-                        // the updating query string
                         var updateQueryStr = "UPDATE products SET stock_quantity = " + (productInfo.stock_quantity - quantity) + " WHERE item_id = " + item
-                            // console.log('updateQueryStr = ' + updateQueryStr);
 
-                        // Update the inventory
-                        connection.query(updateQueryStr, function(err, data) {
+                        connection.query(updateQueryStr, function (err, data) {
                             if (err) throw err;
 
                             console.log("Your order has been placed!");
@@ -99,8 +86,6 @@ let purchase = () => {
                             console.log(" - - - - - - - - - - - - - - - ")
                             console.log("To shop again with us please input 'node bamazonCustomer.js' into your command line again.")
                             console.log("\n")
-
-                            // End the database connection and close the app
                             connection.end();
                         })
                     } else {
@@ -109,14 +94,9 @@ let purchase = () => {
                         console.log("Please modify your order or select another item.")
                         console.log("\n")
 
-                        // After 3 seconds display the inventory again so that the customer can make a new selcetion.
-                        setTimeout(function() { displayInventory() }, 3000)
+                        setTimeout(function () { displayInventory() }, 3000)
                     }
-
-
                 }
             })
-
-
         })
 }
